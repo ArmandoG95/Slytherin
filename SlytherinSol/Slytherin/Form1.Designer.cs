8b577fbfed6ed4cff5bfa1398a0bb5cb3a2fc912
Emigdio Espinosa <millosespinosa@gmail.com>
2021-03-15 11:21:21 -0600

    Se agregó un PictureBox con nombre pbFondo y se modifico

--- SlytherinSol/Slytherin/Form1.Designer.cs
+++ SlytherinSol/Slytherin/Form1.Designer.cs
@@ -28,22 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbFondo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFondo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbFondo
+            // 
+            this.pbFondo.BackColor = System.Drawing.Color.Gray;
+            this.pbFondo.Location = new System.Drawing.Point(13, 13);
+            this.pbFondo.Name = "pbFondo";
+            this.pbFondo.Size = new System.Drawing.Size(541, 560);
+            this.pbFondo.TabIndex = 0;
+            this.pbFondo.TabStop = false;
             // 
             // FrmSlytherin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(907, 623);
+            this.Controls.Add(this.pbFondo);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmSlytherin";
             this.Text = "Slytherin";
             this.Load += new System.EventHandler(this.FrmSlytherin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFondo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbFondo;
     }
 }
 
