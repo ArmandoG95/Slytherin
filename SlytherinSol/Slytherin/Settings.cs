af69674479f4e80ed0fb9d702864d2855752c6f9
KARLA PAULINA CABRERA ENRÍQUEZ <pcabrera.umad@gmail.com>
2021-03-15 13:41:14 -0600

    Se crearon las propiedades de la clase "settings" y se establecieron sus valores por defecto

--- SlytherinSol/Slytherin/Settings.cs
+++ SlytherinSol/Slytherin/Settings.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Slytherin
 {
-    class Settings
+    public enum Directions
     {
+        Left,
+        Right,
+        Up,
+        Down,
+    };
+    class Settings
+    { 
+        public static int Width { get; set;}
+        public static int Height { get; set; }
+        public static int Speed { get; set; }
+        public static int Score { get; set; }
+        public static int Points { get; set; }
+        public static bool GameOver { get; set; }
+        public static Directions direction { get; set; }
+
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            Speed = 20;
+            Score=0;
+            Points = 100;
+            GameOver = false;
+            direction = Directions.Down;
+        }
     }
 }
