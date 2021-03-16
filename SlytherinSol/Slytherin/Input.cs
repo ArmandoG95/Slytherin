f078ae162146308eb9ca8a0b5837c9784039d834
ARMANDO GONZALEZ JR <sixmit@hotmail.com>
2021-03-15 21:16:28 -0600

    Se agrego funcionalidad a la clase Input

--- SlytherinSol/Slytherin/Input.cs
+++ SlytherinSol/Slytherin/Input.cs
@@ -11,5 +11,17 @@ namespace Slytherin
     class Input
     {
      private static Hashtable keyTable = new Hashtable();
+     public static bool KeyPress(Keys key)
+        {
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+            return (bool)keyTable[key];
+        }
+        public static void changeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
     }
 }
